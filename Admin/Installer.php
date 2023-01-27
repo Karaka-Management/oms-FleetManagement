200e3152f48139a0518ced9f4d87e71ce1f861ed
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-01-27 22:12:09 +0100

    phpstan, phpcs, phpunit fixes

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -14,13 +14,7 @@ declare(strict_types=1);
 
 namespace Modules\FleetManagement\Admin;
 
-use phpOMS\Application\ApplicationAbstract;
-use phpOMS\Config\SettingsInterface;
-use phpOMS\Message\Http\HttpRequest;
-use phpOMS\Message\Http\HttpResponse;
 use phpOMS\Module\InstallerAbstract;
-use phpOMS\Module\ModuleInfo;
-use phpOMS\Uri\HttpUri;
 
 /**
  * Installer class.
@@ -39,12 +33,4 @@ final class Installer extends InstallerAbstract
      * @since 1.0.0
      */
     public const PATH = __DIR__;
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function install(ApplicationAbstract $app, ModuleInfo $info, SettingsInterface $cfgHandler) : void
-    {
-        parent::install($app, $info, $cfgHandler);
-    }
 }
