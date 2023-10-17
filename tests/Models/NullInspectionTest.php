23fa57f22c3a8d5e27c3baf5c55338ad77d2760a
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullInspectionTest.php
+++ tests/Models/NullInspectionTest.php
@@ -47,6 +47,6 @@ final class NullInspectionTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullInspection(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
